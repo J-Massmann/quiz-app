589e2750348536115e97bdb27dcc9a537a981aaa
Justus Massmann <justusmassmann@online.de>
2022-01-25 17:11:35 +0100

    some changes

--- styles/components/index.js
+++ styles/components/index.js
@@ -1,8 +1,12 @@
 function changeImg() {
-  const image = document.querySelector('[data-js="button-bookmark"]');
-  if (image.src == "http://127.0.0.1:3000//images/bookmark-regular.svg") {
-    image.src = "http://127.0.0.1:3000//images/bookmark-fill.svg";
+  const image = document.querySelector('[data-js="button-image"]');
+  console.log(image.src);
+  if (image.src.includes("/images/bookmark-regular.svg")) {
+    image.src = "/images/bookmark-fill.svg";
   } else {
-    image.src = "http://127.0.0.1:3000//images/bookmark-regular.svg";
+    image.src = "/images/bookmark-regular.svg";
   }
 }
+
+const buttonElement = document.querySelector('[data-js="button-bookmark"]');
+buttonElement.addEventListener("click", changeImg);
