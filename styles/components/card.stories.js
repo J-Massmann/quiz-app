c55f46d48283b6c8f8b2ab30a4e05f897b66b20e
Justus Massmann <justusmassmann@online.de>
2022-01-27 14:05:18 +0100

    Add prettier and format a couple of files

--- styles/components/card.stories.js
+++ styles/components/card.stories.js
@@ -1,7 +1,7 @@
-import html from "./card.stories.html";
+import html from './card.stories.html';
 
 export default {
-  title: "Components/card",
+  title: 'Components/card',
 };
 
 export const Default = () => html;
