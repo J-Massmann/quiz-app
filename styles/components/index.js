54d350ff0091cd162ca7cbdb9247b73d462095c0
Justus Massmann <justusmassmann@online.de>
2022-01-27 20:18:18 +0100

    Update button show answer / hide answer

--- styles/components/index.js
+++ styles/components/index.js
@@ -9,3 +9,16 @@ function changeImg() {
     image.src = "/images/bookmark-regular.svg";
   }
 }
+
+const showAnswer = document.querySelector('[data-js="show-answer"]');
+const textElement = document.querySelector('[data-js="hide-text"]');
+
+showAnswer.addEventListener("click", () => {
+  if (showAnswer.textContent.trim() === "show answer") {
+    textElement.classList.remove("card__answer-hide");
+    showAnswer.textContent = "hide answer";
+  } else {
+    textElement.classList.add("card__answer-hide");
+    showAnswer.textContent = "show answer";
+  }
+});
