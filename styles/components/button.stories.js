6c3b2db1dd570caa3c326da8619f6f5e9cc09600
Justus Massmann <justusmassmann@online.de>
2022-01-27 22:34:28 +0100

    Update storybook

---
+++ styles/components/button.stories.js
@@ -0,0 +1,9 @@
+import "./button.css";
+
+export default {
+  title: "Components/button",
+};
+
+export const bookmark = () => `<button class="button--bookmark">click</button>`;
+export const showAnswer = () =>
+  `<button class="buttom--show-answer">click</button>`;
